5d9864b2cf5db803212b83febf0ad7f6f7cabcf8
saikixvz <97126732+saikixvz@users.noreply.github.com>
2022-01-04 15:09:40 -0500

    Update help.js

--- src/help.js
+++ src/help.js
@@ -1,116 +1,107 @@
 const help = (prefix) => {
 	return `
-
-⌜ _*ShanBot by shanduy*_ ⌟  
-
-◉ *INFORMACION*
-   ○ Comando: ⌜ ${prefix} ⌟
-   ○ Creador: shanduy™ 
-   ○ Cómo instalar el bot: https://youtu.be/HxZYNgW9aI8
-   ○ Mi Instagram: https://www.instagram.com/thepavos
-
-◉ *COMUNICADO*
-Nuevo video de cómo instalar el bot :)
-https://youtu.be/HxZYNgW9aI8
-
-◉ *PARA USAR EL BOT*
-Registrate con el comando ${prefix}daftar y tu nombre
-
-◉ *NUEVOS COMANDOS*
-○ ${prefix}banmenu
-Banear a personas el uso del bot
-○ ${prefix}levelmenu
-Level para tus grupos
-○ ${prefix}antimenu
-Su nombre lo dice todo (antilink)
-○ ${prefix}otak
-Monas chinas
-○ ${prefix}shantera
-Interactua con el bot
-
-◉ *NUEVOS MENUS*
-○ ${prefix}juegos
-Diviértete con tus amigos :)
-○ ${prefix}desmenu
-Descargar musica y videos
-○ ${prefix}version
-Conoce la versión de tu bot
-○ ${prefix}welmenu
-Comando de bienvenida solo grupos
-
-◉ *RESUELVE TUS DUDAS*
-  ║
-  ╠ ○ ${prefix}creador
-  ╚ Dudas o problemas aqui
-
-◉ *CREAR STICKERS*
-  ║
-  ╠ ○ ${prefix}sticker
-  ╠ ○ ${prefix}attp
-  ╠ Mas un texto corto
-  ╠ ○ ${prefix}stickergif
-  ╚ 6 segundos de video
-
-◉ *CONVERTIDORES*
-  ║
-  ╠ ○ ${prefix}toimg
-  ╠ De sticker a JPG
-  ╠ ○ ${prefix}tomp3
-  ╚ De video a MP3
-
-◉ *AUDIO*
-  ║
-  ╠ ○ ${prefix}idioma
-  ╚ ○ ${prefix}tts es (mas texto)
-
-◉ *OTROS*
-  ║
-  ╠ ○ ${prefix}wame
-  ╠ Link de Whatsapp
-  ╠ ○ ${prefix}qrcode
-  ╚ Coloca un texto
-
-◉ *GRUPOS*
-  ║
-  ╠ ○ ${prefix}unir
-  ╠ Unir a una persona al grupo
-  ╠ ○ ${prefix}fgc
-  ╠ Cambiar foto del grupo
-  ╠ ○ ${prefix}ngc
-  ╠ Cambiar nombre del grupo
-  ╠ ○ ${prefix}dgc
-  ╠ Cambiar descripcion del grupo
-  ╠ ○ ${prefix}closegc
-  ╠ Cerrar el grupo solo admins
-  ╠ ○ ${prefix}opengc
-  ╠ Abrir grupo solo admins
-  ╠ ○ ${prefix}promote
-  ╠ Dar admin a un miembro
-  ╠ ○ ${prefix}demote
-  ╠ Quitar el admin
-  ╠ ○ ${prefix}linkgc
-  ╠ Link del grupo
-  ╠ ○ ${prefix}adminlist
-  ╠ Nombra a los admins del grupo
-  ╠ ○ ${prefix}todos
-  ╚ Nombra a todos los del grupo
-
-Para usar estas funciones el bot necesita admin
-  
-◉ *NSWF* 
-  ║
-  ╚ ○ ${prefix}nsfwmenu
-
-Para activar los NSFW coloque el siguiente comando ${prefix}nsfw 1 y para desactivar los NSFW coloque ${prefix}nsfw 0
-
-
-No te olvides de seguirme en instagram flaco ;)
-
-
-_*by shanduy*_
-
-
-⌜ *ごきげんよう :)* ⌟ 
+▋▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄
+▋┋        ┨ ☠Saiki-Bot☠ ┠
+▋┋ 
+▋┋ Hola,por favor registrarte con:
+▋┋ /daftar y tú nombre
+▋┋ /reg y tú nombre
+▋┋ /rg y tú nombre 
+▋┋ /verify y tú nombre 
+▋┋ /registrar y tú nombre 
+▋┋ Creador: *☂︎𝓓𝓪𝓻𝓵𝔂𝓷☂︎*
+▋┋ Comando: ⌜ ${prefix} ⌟
+▋┋
+▋▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄
+▋┋ ╭─────────────╮
+■█   ✰*CREAR STICKERS*✰
+▋┋ ╰─────────────╯
+▋┋ *Imagen*
+▋┋ ${prefix}sticker
+▋┋ *texto*
+▋┋ ${prefix}attp
+▋┋ *Gif*
+▋┋ ${prefix}stickergif
+▋┋ *6 segundos de video*
+▋┋
+▋┋ ╭─────────────╮
+■█   ✰*CONVERTIDORES*✰
+▋┋ ╰─────────────╯
+▋┋ *De sticker a JPG*
+▋┋ ${prefix}toimg
+▋┋ *De video a MP3*
+▋┋ ${prefix}tomp3
+▋┋
+▋┋ ╭──────────────╮
+■█ ✰*AUDIOS Y DESCARGAS*✰
+▋┋ ╰──────────────╯
+▋┋ *Descargar musica de YT*
+▋┋ ${prefix}desmenu
+▋┋ *Audio*
+▋┋ ${prefix}tts es (mas texto)
+▋┋ ${prefix}idioma
+▋┋ *Monas chinas*
+▋┋ ${prefix}otak
+▋┋ *Interactua con el bot*
+▋┋ ${prefix}shantera
+▋┋
+▋┋ ╭──────────────╮
+■█       ✰*OTROS*✰
+▋┋ ╰──────────────╯
+▋┋ *Juega con el bot*
+▋┋ ${prefix}juegos
+▋┋ *Link de Whatsapp*
+▋┋ ${prefix}wame
+▋┋ *Coloca un texo*
+▋┋ ${prefix}qrcode
+▋┋ *Creador del bot*
+▋┋ ${prefix}creador
+▋┋ *Nswfmenu*
+▋┋ ${prefix}nsfwmenu
+▋┋
+▋┋ ╭──────────────╮
+■█  ✰*GRUPOS Y ADMINS*✰
+▋┋ ╰──────────────╯
+▋┋ *Cerrar el grupo solo admins*
+▋┋ ${prefix}close o cerrar
+▋┋ *Abrir grupo solo admins*	
+▋┋ ${prefix}open o abrir
+▋┋ *Eliminar un miembro*
+▋┋ ${prefix}kick o alv
+▋┋ *Dar admin a un miembro*
+▋┋ ${prefix}promote
+▋┋ *Quitar admin a un miembro*
+▋┋ ${prefix}demote
+▋┋ *Comando de bienvenida solo grupos*
+▋┋ ${prefix}welmenu
+▋┋ *Menu antilink*
+▋┋ ${prefix}antimenu
+▋┋ *Link del grupo*
+▋┋ ${prefix}linkgc
+▋┋ *Nombra admins del grupo*
+▋┋ ${prefix}adminlist
+▋┋ *Mencionar a todos*
+▋┋ ${prefix}hidetag o todos
+▋┋ *Agregar un miembro con el numero*
+▋┋ ${prefix}unir y el numero
+▋┋ *Cambiar foto del grupo*
+▋┋ ${prefix}fotog
+▋┋ *Cambiar descripcion del grupo*
+▋┋ ${prefix}dgc
+▋┋ *Cambiar nombre del grupo*
+▋┋  ${prefix}name
+▋┋ ╭──────────────╮
+■█  ✰*OWNER*✰
+▋┋ ╰──────────────╯
+▋┋ *Actualizar el bot*
+▋┋ ${prefix}update
+▋┋ *Apagar el bot*
+▋┋ ${prefix}off
+▋┋ *Ver los chats del bot*
+▋┋ ${prefix}grupos
+▋┋
+▋┋ *SAIKI*
+▋▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄
 `
 }
 
